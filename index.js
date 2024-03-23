3837224b6ffd3e4a8e343216e91f9c0c4b299dcf
Shem Karafa <karafashem@gmail.com>
2024-03-23 18:44:24 +0300

    create index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    if (typeof customerName === 'string') {
+        customerName = customerName.toUpperCase();
+    }
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const LeastFavoriteCustomer = 'Lee'
+
+function changeLeastFavoriteCustomer(newCustomer) {
+    LeastFavoriteCustomer = newCustomer;
+}
